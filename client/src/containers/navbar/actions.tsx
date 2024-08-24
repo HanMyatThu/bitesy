6820c6dbf973ae6227c38b94ae9fcb6b2c7bba61
Han Myat Thu <hanmyatthu111@gmail.com>
2024-08-24 09:58:45 +0200

    install usehook and change css based on device

--- client/src/containers/navbar/actions.tsx
+++ client/src/containers/navbar/actions.tsx
@@ -1,22 +1,31 @@
 import { NotepadText, ShoppingCart } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
 
 import { UserAvatar } from "./avatar";
 import { ToggleTheme } from "@/components/darktheme/toggle-theme";
 import { IconButton } from "@/components/common/icon-button";
 import { ToolTipHint } from "@/components/common/tooltip-hint";
 import { LanguagePicker } from "@/components/language/language-picker";
+import { cn } from "@/lib/utils";
 
 export const Actions = () => {
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
   return (
     <div className="ml-4 flex items-center justify-end gap-x-2 lg:ml-0">
-      <div className="flex items-center gap-x-4">
+      <div
+        className={cn(
+          "flex items-center justify-center",
+          isMobile ? "gap-x-2" : "gap-x-4",
+        )}
+      >
         <div className="hidden md:flex lg:flex xl:flex">
           <ToolTipHint label="Orders" side="bottom">
             <IconButton href="/" icon={NotepadText} />
           </ToolTipHint>
         </div>
-        <ToggleTheme />
         <IconButton href="/" icon={ShoppingCart} />
+        <ToggleTheme />
         <LanguagePicker />
         <div className="hidden md:flex lg:flex xl:flex">
           <UserAvatar imageUrl="/images/logo.png" isActive size="default" />
